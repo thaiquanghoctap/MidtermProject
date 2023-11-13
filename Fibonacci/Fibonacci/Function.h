31be47b81c8483efdc798ed0c6aad853bf1bafb1
DianaTeresa <20120238@student.hcmus.edu.vn>
2023-11-13 20:54:20 +0700

    Code

--- Fibonacci/Fibonacci/Function.h
+++ Fibonacci/Fibonacci/Function.h
@@ -1,9 +1,32 @@
-#pragma once
+// #pragma once
 
 #include <iostream>
 #include <ctime>
+#include <vector>
+
+class Timer
+{
+    public: 
+        time_t start;
+        time_t end;
+
+    void start_timer()
+    {
+        this->start = std::time(NULL);
+    }
+
+    void end_timer()
+    {
+        this->end = std::time(NULL);
+    }
+
+    void count_time() 
+    { 
+        std::cout <<"Time spent: " << difftime(start, end) << " seconds" << std::endl;
+    }
+};
 
 int fibonacci_recursive();
 int fibonacci_loop();
-int fibonacci_dynamic();
+void fibonacci_dynamic(int n, std::vector<long long> &fibo);
 int fibonacci_matrix();
--- Fibonacci/Fibonacci/Main.cpp
+++ Fibonacci/Fibonacci/Main.cpp
@@ -1,8 +1,33 @@
 #include "Function.h"
+#include <vector>
+#include <iostream>
+using namespace std;
 
 int main()
 {
 	int n = 0;
 
+	while(true)
+	{
+		cin >> n;
+		if ( n < 0) 
+			cout << "Invalid n";
+		else break;
+	}
+
+	// Recursive
+
+
+	// Loop
+
+
+	// Dynamic
+	vector<long long> fibo {0,1};
+	fibonacci_dynamic(n, fibo);
+    cout << n << "th Fibonacci: " << fibo[n] << endl;
+
+	// Matrix
+
+    
 	return 0;
 }
